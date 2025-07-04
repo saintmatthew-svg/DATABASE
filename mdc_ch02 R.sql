6af5f82149eae4e2c01d8e1fe7ac176e98cfcce8
saintmatthew-svg <onyekachimatthew00@gmail.com>
2025-07-04 11:56:36 +0100

    commited

--- mdc_ch02 R.sql
+++
@@ -1,6 +0,0 @@
-SELECT c.LastName, c.FirstName, c.Phone
-FROM customer c
-JOIN invoice i ON c.CustomerID = i.CustomerID
-JOIN invoice_item ii ON i.CustomerID = ii.ItemNumber
-WHERE ii.Item = 'Dress Shirt'
-ORDER BY c.LastName ASC, c.FirstName DESC;
